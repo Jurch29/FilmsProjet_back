a77d2f6d08c59263948d09f9b9f41c91a9b73739
Jurch29 <julien.rch.29@gmail.com>
2019-12-13 13:51:58 +0100

    First commit

---
+++ src/main/java/bzh/jap/main/FilmsProjetApplication.java
@@ -0,0 +1,13 @@
+package bzh.jap.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmsProjetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmsProjetApplication.class, args);
+	}
+
+}
