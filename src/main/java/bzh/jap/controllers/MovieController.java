ba9580c801211e38e15250005dd5ffdffd329704
Julien <Julien@DESKTOP-QS00GLD>
2020-02-28 20:44:05 +0100

    Synchro total v1.0

--- src/main/java/bzh/jap/controllers/MovieController.java
+++ src/main/java/bzh/jap/controllers/MovieController.java
@@ -10,7 +10,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import bzh.jap.models.Actor;
+import bzh.jap.models.Author;
+import bzh.jap.models.Category;
 import bzh.jap.models.Movie;
+import bzh.jap.repository.ActorRepository;
+import bzh.jap.repository.AuthorRepository;
+import bzh.jap.repository.CategoryRepository;
 import bzh.jap.repository.MovieRepository;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -21,6 +27,15 @@ public class MovieController {
 	@Autowired
 	private MovieRepository movieRepository;
 	
+	@Autowired
+	private ActorRepository actorRepository;
+	
+	@Autowired
+	private AuthorRepository authorRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	@GetMapping("/movies")
 	public List<Movie> retrieveAllMovies() {
 		return movieRepository.findAll();
@@ -29,6 +44,21 @@ public class MovieController {
 	@GetMapping("/movie/{id}")
 	public Optional<Movie> getMovieById(@PathVariable long id) {
 		return movieRepository.findById(id);
+	}
+	
+	@GetMapping("/actors")
+	public List<Actor> getAllActors() {
+		return actorRepository.findAll();
+	}
+	
+	@GetMapping("/authors")
+	public List<Author> getAllAuthors() {
+		return authorRepository.findAll();
+	}
+	
+	@GetMapping("/categories")
+	public List<Category> getAllcategories() {
+		return categoryRepository.findAll();
 	}
 
 }
