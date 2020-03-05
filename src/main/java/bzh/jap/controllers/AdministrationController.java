096a177871c9dd6bf3cda8c1a82ca1023f3c1e0d
Julien <Julien@DESKTOP-QS00GLD>
2020-03-05 17:47:36 +0100

    Update Movie + add Movie

--- src/main/java/bzh/jap/controllers/AdministrationController.java
+++ src/main/java/bzh/jap/controllers/AdministrationController.java
@@ -1,9 +1,13 @@
 package bzh.jap.controllers;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import javax.validation.Valid;
 
@@ -23,11 +27,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import bzh.jap.models.ERole;
 import bzh.jap.models.Movie;
+import bzh.jap.models.MovieDescription;
 import bzh.jap.models.Role;
 import bzh.jap.models.User;
 import bzh.jap.payload.AddMovieRequest;
-import bzh.jap.payload.LoginRequest;
 import bzh.jap.payload.MessageResponse;
+import bzh.jap.repository.MovieDescriptionRepository;
 import bzh.jap.repository.MovieRepository;
 import bzh.jap.repository.RoleRepository;
 import bzh.jap.repository.UserRepository;
@@ -48,6 +53,9 @@ public class AdministrationController {
 	
 	@Autowired
 	private RoleRepository roleRepository;
+	
+	@Autowired
+	private MovieDescriptionRepository movieDescriptionRepository;
 	
 	@DeleteMapping("/deleteuser/{id}")
 	@Cascade(CascadeType.DELETE)
@@ -107,6 +115,33 @@ public class AdministrationController {
 		return ResponseEntity.ok(new MessageResponse("Movie deleted"));
 	}
 	
+	@PostMapping("/updatemovie")
+	@PreAuthorize("hasRole('ADMIN')")
+	public ResponseEntity<?> updateMovie(@RequestBody Map<String, Object> lookupRequestObject) {
+		long movieId = ((Number) lookupRequestObject.get("movieId")).longValue();
+		double price = Double.valueOf((String)lookupRequestObject.get("moviePrice"));
+		int duration = Integer.valueOf((String)lookupRequestObject.get("movieDuration"));
+		Timestamp timestamp = null;
+		try {
+			SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+			SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+			Date date = inputFormat.parse((String)lookupRequestObject.get("movieDate"));
+			date = outputFormat.parse(outputFormat.format(date));
+		    timestamp = new java.sql.Timestamp(date.getTime()+TimeUnit.DAYS.toMillis(1));
+		} catch(Exception e) { //this generic but you can control another types of exception
+		    System.out.println(e);
+		}
+		Movie movie = movieRepository.findById(movieId).get();
+		movie.setMovieTitle((String)lookupRequestObject.get("movieTitle"));
+		movie.setMoviePrice(price);
+		movie.setMovieDate(timestamp);
+		movie.setMovieImagePath((String)lookupRequestObject.get("movieImagePath"));
+		movie.setMovieTrailerPath((String)lookupRequestObject.get("movieTrailerPath"));
+		movie.setMovieDuration(duration);
+		movieRepository.save(movie);
+		return ResponseEntity.ok(new MessageResponse("ok"));
+	}
+	
 	@PostMapping("/addmovie")
 	@PreAuthorize("hasRole('ADMIN')")
 	public ResponseEntity<?> addNewMovie(@Valid @RequestBody AddMovieRequest addMovieRequest) {
@@ -114,6 +149,10 @@ public class AdministrationController {
 		movie.setMovieFilePath("/localFile");
 		movie.setMovieIsDeleted(false);
 		movieRepository.save(movie);
+		MovieDescription movieDescription = new MovieDescription();
+		movieDescription.setMovieId(String.valueOf(movie.getMovieId()));
+		movieDescription.setMovieDescription(addMovieRequest.getSynopsis());
+		movieDescriptionRepository.save(movieDescription);
 		return ResponseEntity.ok(new MessageResponse("ok"));
 	}
 	
--- src/main/java/bzh/jap/payload/AddMovieRequest.java
+++ src/main/java/bzh/jap/payload/AddMovieRequest.java
@@ -5,12 +5,22 @@ import bzh.jap.models.Movie;
 public class AddMovieRequest {
 	
 	private Movie movie;
+	private String synopsis;
 	
 	public Movie getMovie() {
 		return movie;
 	}
+	
 	public void setMovie(Movie movie) {
 		this.movie = movie;
 	}
-
+	
+	public String getSynopsis() {
+		return synopsis;
+	}
+	
+	public void setSynopsis(String synopsis) {
+		this.synopsis = synopsis;
+	}
+	
 }
