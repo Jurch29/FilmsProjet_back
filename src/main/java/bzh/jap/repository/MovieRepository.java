08cd0d5ecc2ab813f8892bb17ad6865e3036be63
Jurch29 <julien.rch.29@gmail.com>
2020-03-06 14:40:03 +0100

    Mark v2

--- src/main/java/bzh/jap/repository/MovieRepository.java
+++ src/main/java/bzh/jap/repository/MovieRepository.java
@@ -14,4 +14,7 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
 	
 	@Query(value = "SELECT * FROM Movie ORDER BY movie_mark DESC LIMIT 5", nativeQuery = true)
 	List<Movie> findTopFiveMovies();
+	
+	@Query("SELECT ROUND(AVG(MovieUserMark.movie_user_mark_mark), 1) AS mark FROM MovieUserMark WHERE MovieUserMark.movie_id =?1")
+	int getAverageMark(Integer movieId);
 }
